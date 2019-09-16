3953d99131761d9ad7885b9caa8239316e421e26
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-16 14:40:13 -0300

    merge

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by jt on 6/19/17.
+ */
 @Controller
 public class RecipeController {
 
@@ -14,8 +17,10 @@ public class RecipeController {
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
+
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
