e5ad37f6290ea11fe6570c562a79b05391bc9199
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-17 17:18:54 -0300

    Implemented delete ingredient

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -75,7 +75,7 @@ public class RecipeControllerTest {
                 .param("description", "some string")
         )
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/recipe/2/show/"));
+                .andExpect(view().name("redirect:/recipe/2/show"));
     }
 
     @Test
