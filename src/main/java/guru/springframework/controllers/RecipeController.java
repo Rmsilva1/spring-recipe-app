9c625f4d598d309ae91b1c38b9a413ec0bb8fa89
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-17 14:33:23 -0300

    Added create of ingredient

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -20,15 +20,19 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
+
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
-    public String newRecipe(Model model) {
+    public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
+
         return "recipe/recipeform";
     }
 
@@ -36,20 +40,22 @@ public class RecipeController {
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
+        return  "recipe/recipeform";
     }
 
-    @PostMapping
-    @RequestMapping("recipe")
+    @PostMapping("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-        return "redirect:/recipe/" + savedCommand.getId() + "/show/";
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
     @GetMapping
     @RequestMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
+
         log.debug("Deleting id: " + id);
+
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
